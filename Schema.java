000c8aa0cee2bd483c6bc87f3be0a61203369fe4
SamMac55 <samanthamac055@gmail.com>
2026-04-21 09:54:11 -0500

    starting schema funcitonality

---
+++ Schema.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+public class Schema {
+    List<Table> tables = new ArrayList<>();
+
+    public boolean hasTable(String name) {
+        for (Table t : tables) {
+            if (t.table_name.equals(name))
+                return true;
+        }
+        return false;
+    }
+
+    public static class Table{
+        String table_name;
+        List<Attribute> attributes = new ArrayList<>();
+
+        public Table(String name) {
+            this.table_name = name;
+        }
+        public boolean hasAttribute(String name) {
+            for (Attribute a : attributes) {
+                if (a.attr_name.equals(name))
+                    return true;
+            }
+            return false;
+        }
+        public boolean references(Table other) {
+            for (Attribute a : attributes) {
+                if (a.hasConstraint("references " + other.table_name)) //need to check type eventually
+                    return true;
+            }
+            return false;
+        }
+    }
+    public static class Attribute{
+        String attr_name;
+        String type;
+        List<String> constraints;
+        public Attribute(String name, String type, ArrayList<String> constraints) {
+            this.attr_name = name;
+            this.type = type;
+            this.constraints = constraints;
+        }
+        public boolean hasConstraint(String constraint) {
+            return constraints.contains(constraint);
+        }
+    }
+}
---
+++ schema_grammar.g4
@@ -0,0 +1,36 @@
+grammar schema_grammar;
+import base_lexer;
+//note to self the next step is to create the visitor for this and generate the symbol table + add semantic checking
+//used for parsing the .fullschema command from sqlite so we can use it for semantic processing
+//program: fullschema EOF;
+
+
+createTable: CREATE TABLE tablename=ID '(' columnDef (',' columnDef)* ')' ';';
+
+
+
+columnDef: attributeName=ID dataType? constraint*
+        | foreignKey;
+
+dataType: INTEGERKEYWORD
+        |   REAL
+        |   TEXT
+        ;
+
+constraint: NOT NULL
+        |   PRIMARY KEY
+        ;
+
+foreignKey: FOREIGN KEY REFERENCES refTable=ID '(' refColumn=ID ')' ;
+
+CREATE: 'CREATE';
+TABLE: 'TABLE';
+NOT: 'NOT';
+NULL: 'NULL';
+PRIMARY: 'PRIMARY';
+KEY: 'KEY';
+INTEGERKEYWORD: 'INTEGER';
+REAL: 'REAL';
+TEXT: 'TEXT';
+FOREIGN: 'FOREIGN';
+REFERENCES: 'REFERENCES';
